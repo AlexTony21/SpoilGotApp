e0bfd21337d0202813183accf3497a1c9a8c4a2c
Alexandre Huynh <alexandretony.huynh@hotmail.fr>
2020-06-19 16:38:52 +0200

    Clean

--- src/main/java/com/fdorval/spoilgot/dao/impl/GotCharacterRepository.java
+++
@@ -1,6 +0,0 @@
-package com.fdorval.spoilgot.dao.impl;
-import org.springframework.data.repository.CrudRepository;
-
-import com.fdorval.spoilgot.dao.model.GotCharacterBack;
-
-public interface GotCharacterRepository extends CrudRepository<GotCharacterBack, Integer> {}
--- src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS GotCharacterBack;
-
-CREATE TABLE GotCharacterBack (
-  id INT  DEFAULT NULL,
-  name VARCHAR(250) NOT NULL,
-  killedby INT DEFAULT NULL,
-  killedinseason VARCHAR(250) DEFAULT NULL
-);
